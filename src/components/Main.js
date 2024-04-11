73bfaf16aa85845f7a6cae5994ae8a16ab5133bd
delmarNK <delmarjinku3@gmail.com>
2024-04-12 02:31:34 +0800

    created the event components and its pages

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router"
+import Events from "../pages/Events"
+import Show from "../pages/Show"
+import Update from "../pages/Update"
+
+function Main() {
+  return (
+    <div>
+        <Routes>
+            <Route path="/" element={<Events />}/>
+            <Route path="/id" element={<Show />}/>
+            <Route path="/update/:id" element={<Update />}/>
+        </Routes>
+    </div>
+  )
+}
+
+export default Main
---
+++ src/pages/Events.js
@@ -0,0 +1,45 @@
+import { useState,useEffect } from "react"
+
+function Events() {
+    const [events, setEvent] = useState(null)
+    const URL = process.env.REACT_APP_API_URL + '/event'
+
+    function getEvent(){
+        try{
+            fetch(URL)
+            .then((res)=> res.json())
+            .then((data)=>setEvent(data))
+        } catch(error){
+            console.log(error)
+        }
+    }
+    useEffect(()=>{
+        getEvent()
+    },[])
+
+    function loading(){
+        return <section className="peopleList"><h1>Hold on a sec...
+            <span>
+                <img className="spinner" src="https://freesvg.org/img/1544764567.png" />
+            </span>
+         </h1>
+        </section>
+    }
+    function loaded(){
+        return (
+            <div className="events">
+                {events.map((event)=>{
+                    <div>
+                        <h1>{event.title}</h1>
+                        <img src={event.image} className="eventImage"/>
+                        <p>{event.content}</p>
+                        <p>{event.location} {event.time}</p>
+                    </div>
+                })}
+            </div>
+        )
+    }
+    events ? loaded() : loading()
+}
+
+export default Events
