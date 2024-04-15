9117b0947bccf4b2cd10fc6ba228b8bc45df8d2f
delmarNK <delmarjinku3@gmail.com>
2024-04-15 21:19:55 +0800

    updated the events page css

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Main from './components/Main';
 function App() {
   return (
     <div className="App">
-      <h1 className='appName'>CCN</h1>
-      <hr/>
+      <div className='header'>
+        <h1 className='appName'>CCN</h1>
+        <hr style={{borderColor: 'black'}}/>
+      </div>
       <Main />
     </div>
   );
