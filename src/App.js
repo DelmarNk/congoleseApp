110a3c2c58fb02b1b0324bda7f1b7973e88e0e25
delmarNK <delmarjinku3@gmail.com>
2024-04-24 15:32:26 +0800

    updated landing page css

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div className='header'>
         <div className='headContent'>
           <h1 className='appName'>CCN</h1>
-          <img className='flag' src='https://upload.wikimedia.org/wikipedia/commons/9/92/Flag_of_the_Republic_of_the_Congo.svg' />
+          {/* <img className='flag' src='https://upload.wikimedia.org/wikipedia/commons/9/92/Flag_of_the_Republic_of_the_Congo.svg' /> */}
         </div>
         <hr style={{borderColor: 'black'}}/>
       </div>
