3f0c623d085986d1a84d38045959794178791f1b
delmarNK <delmarjinku3@gmail.com>
2024-04-27 01:34:19 +0800

    updateded landing page css

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 function LandingPage() {
   return (
     <div className='landingPage'>
+        <img className='backgroundImage' src='https://w0.peakpx.com/wallpaper/61/461/HD-wallpaper-red-sky-at-night-dawn-sky-sea-ocean.jpg'/>
         <div className='empty'></div>
         <div className='landingContent'>
             <p className='tagline'>Want to be updated about all the congolese activities in Nanjing?</p>
